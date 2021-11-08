1d2caa16bf1dbb3d74ff31c0ef459f93a2711d21
Renato Barros <renatobarros123@outlook.com.br>
2021-11-08 04:05:28 -0300

    finalizing screen structure and logic

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# CustomObjectListing
+# Teste de desenvolvimento Ex. 14
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.1.
+*O layout foi desenvolvido em Angular
+### Para executar o projeto:
 
-## Development server
+> Verifique se você está executando o node 12.4.0 com o camando node -v em uma janela de terminal / console. As versões mais antigas produzem erros, mas as versões mais recentes funcionam.
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+1. npm install
+2. npm start
 
-## Code scaffolding
+# Proposta:
+A partir da página de listagem de filmes desenvolvida, customize a interface aplicando conceitos ergonômicos e de web design a fim de deixa-la funcional e atrativa.
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+# Resultado: 
 
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+![Captura de tela 2021-11-05 183709](https://user-images.githubusercontent.com/42192877/140581615-19086a9b-b68d-4b4e-8b25-5e3b8fd48d2b.png)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
+
+interface Movie {
+  nome: string;
+  ano: string;
+  diretor: string;
+  genero: string;
+  generos: string[];
+  descricao: string;
+  poster: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'custom-object-listing';
+  private readonly URL = `assets/json/FILMES.JSON`;
+
+  queryField = new FormControl();
+
+  movies$!: Observable<Movie[]>;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.movies$ = this.listMovies();
+
+    this.queryField.valueChanges
+      .pipe(
+        map((value) => value.trim()),
+        filter((value) => value.length > 1),
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe((res) => (this.movies$ = this.searchMovies(res)));
+  }
+
+  listMovies = () =>
+    this.httpClient.get<Movie[]>(this.URL).pipe(
+      map((movies: Movie[]) =>
+        movies.map((movie: Movie) => {
+          movie.generos = movie.genero.split('/');
+          return movie;
+        })
+      )
+    );
+
+  searchMovies = (search: string) =>
+    this.listMovies().pipe(
+      map((movies: Movie[]) =>
+        search.length > 2
+          ? movies.filter((movie: Movie) =>
+              movie.nome
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            )
+          : movies
+      )
+    );
 }
